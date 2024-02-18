41636b233fa5239e69bc930a45039469430c6dc2
DasminX <wojtek.test99@gmail.com>
2024-02-18 16:09:05 +0100

    fix(mail.route): Change /send-mail route to /send

--- src/features/mail/route/mailRoute.ts
+++ src/features/mail/route/mailRoute.ts
@@ -3,6 +3,6 @@ import mailController from "../controller/mailController";
 
 const router = express.Router();
 
-router.post("/send-mail", mailController.sendMail);
+router.post("/send", mailController.sendMail);
 
 export default router;
