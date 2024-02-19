c4d2f832f8c78cf42a18173d70d9592e0284391c
DasminX <wojtek.test99@gmail.com>
2024-02-19 17:21:20 +0100

    tests: add new tests and fix previous

--- src/features/mail/__tests__/mailController.test.ts
+++ src/features/mail/__tests__/mailController.test.ts
@@ -1,7 +1,12 @@
 import { describe, it, expect } from "vitest";
 import { IMailService } from "../service/mailService";
-import { TSendMailRequestBody } from "../model/sendMailRequestBody";
+import {
+  SendMailRequestBody,
+  TSendMailRequestBody,
+} from "../model/sendMailRequestBody";
 import { TSendMailResponse } from "../types/sendMailResponse";
+import { formatZodErrorIssues } from "../../_shared/functions/formatZodErrorIssues";
+import { ZodError } from "zod";
 
 // We're fixing the service - of course it should never return {success: true}, but this is just for a silly illustration of how much
 // it helps to create an interface for testing.
@@ -10,14 +15,26 @@ class MockMailService implements IMailService {
   public sendMail(
     credentials: TSendMailRequestBody
   ): Promise<TSendMailResponse> {
-    return Promise.resolve({ success: true });
+    try {
+      SendMailRequestBody.parse(credentials);
+      return Promise.resolve({ success: true });
+    } catch (e) {
+      return Promise.reject({
+        success: false,
+        error: formatZodErrorIssues((e as ZodError).issues),
+      });
+    }
   }
 }
 
+/* TODO, custom transporter deliverable to mail service (for testing and development and prod environments) */
+
+const mockMailService = new MockMailService();
+
 // Dummy test
 describe("mailService", () => {
-  it("should send mail", async () => {
-    const mailServiceResponse = await new MockMailService().sendMail({
+  it("should send mail if valid credentials provided", async () => {
+    const mailServiceResponse = await mockMailService.sendMail({
       sender: "josue@example.com",
       content:
         "content content content content content content content content content",
@@ -26,5 +43,64 @@ describe("mailService", () => {
 
     expect(mailServiceResponse).toHaveProperty("success");
     expect(mailServiceResponse.success).toBeTruthy();
+  });
+
+  it("should yield an error with invalid sender field if such is invalid", async () => {
+    try {
+      await mockMailService.sendMail({
+        sender: "abc",
+        content:
+          "content content content content content content content content content",
+        subject: "New thing",
+      });
+
+      expect(true).toBe(false);
+    } catch (e) {
+      const sendMailResponseError = e as TSendMailResponse;
+      expect(sendMailResponseError.success).toBeFalsy();
+      expect(sendMailResponseError).toHaveProperty("error");
+      if ("error" in sendMailResponseError) {
+        expect(sendMailResponseError.error).toContain("sender");
+      }
+    }
+  });
+
+  it("should yield an error with invalid content field if such is invalid", async () => {
+    try {
+      await mockMailService.sendMail({
+        sender: "josue@example.com",
+        content: "content",
+        subject: "New thing",
+      });
+
+      expect(true).toBe(false);
+    } catch (e) {
+      const sendMailResponseError = e as TSendMailResponse;
+      expect(sendMailResponseError.success).toBeFalsy();
+      expect(sendMailResponseError).toHaveProperty("error");
+      if ("error" in sendMailResponseError) {
+        expect(sendMailResponseError.error).toContain("content");
+      }
+    }
+  });
+
+  it("should yield an error with invalid subject field if such is invalid", async () => {
+    try {
+      await mockMailService.sendMail({
+        sender: "josue@example.com",
+        content:
+          "content content content content content content content content",
+        subject: "New",
+      });
+
+      expect(true).toBe(false);
+    } catch (e) {
+      const sendMailResponseError = e as TSendMailResponse;
+      expect(sendMailResponseError.success).toBeFalsy();
+      expect(sendMailResponseError).toHaveProperty("error");
+      if ("error" in sendMailResponseError) {
+        expect(sendMailResponseError.error).toContain("subject");
+      }
+    }
   });
 });
