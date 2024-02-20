dd8b6e82c78c15093c863c66a14c636dc6c07ea3
DasminX <wojtek.test99@gmail.com>
2024-02-20 22:40:29 +0100

    refactor(mail.controller): change from function to controller class

--- src/features/mail/controller/mail.controller.ts
+++ src/features/mail/controller/mail.controller.ts
@@ -4,37 +4,41 @@ import { MailService } from "../service/mail.service";
 import { sendMailRequestBodyModel } from "../model/sendMailRequestBody.model";
 import { formatZodErrorIssues } from "../../_shared/functions/formatZodErrorIssues";
 
-export const sendMail = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  // Check incoming request body to fit the shape of model (sendMailRequestBodyModel)
-  const validationResult = sendMailRequestBodyModel.safeParse(req.body);
+export interface MailControllerI {
+  sendMail(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void>;
+}
 
-  // Request body doesn't fit shape of Zod object
-  if (!validationResult.success) {
-    return next(
-      new AppError(formatZodErrorIssues(validationResult.error.issues), 400)
+export class MailController implements MailControllerI {
+  async sendMail(req: Request, res: Response, next: NextFunction) {
+    // Check incoming request body to fit the shape of model (sendMailRequestBodyModel)
+    const validationResult = sendMailRequestBodyModel.safeParse(req.body);
+
+    // Request body doesn't fit shape of Zod object
+    if (!validationResult.success) {
+      return next(
+        new AppError(formatZodErrorIssues(validationResult.error.issues), 400)
+      );
+      /* TODO: consider AppError instances, e.g. ValidationError extends AppError... */
+    }
+
+    // Create or return instance of MailService singleton and call sendMail method on it
+    const sendMailResult = await MailService.getInstance().sendMail(
+      validationResult.data
     );
-    /* TODO: consider AppError instances, e.g. ValidationError extends AppError... */
+
+    // Error in nodemailer
+    if (!sendMailResult.success) {
+      return next(new AppError(sendMailResult.error, 500));
+      /* TODO: InternalError, MailError ? */
+    }
+
+    return res.json(sendMailResult);
   }
-
-  // Create or return instance of MailService singleton and call sendMail method on it
-  const sendMailResult = await MailService.getInstance().sendMail(
-    validationResult.data
-  );
-
-  // Error in nodemailer
-  if (!sendMailResult.success) {
-    return next(new AppError(sendMailResult.error, 500));
-    /* TODO: InternalError, MailError ? */
-  }
-
-  return res.json(sendMailResult);
-};
-
-export default { sendMail };
+}
 
 /* Considerations */
 // You could check if it is somehow possible to make it so that you get mailService as a function parameter, so that the code does not strictly depend on the MailService class,
