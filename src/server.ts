0ecd9f3675db114f73127d412ee3415dc1426261
DasminX <wojtek.test99@gmail.com>
2024-02-22 23:24:29 +0100

    chore(server): add iife call in entry file

--- src/server.ts
+++ src/server.ts
@@ -2,33 +2,35 @@ import dotenv from "dotenv";
 import path from "path";
 import { App } from ".";
 
-process.on("uncaughtException", (err) => {
-  console.log("UNCAUGHT EXCEPTION! Shutting down...");
-  console.log(`${err.name}: ${err.message}`);
-  process.exit(1);
-});
-
-// Nothing in .env, just mocking...
-dotenv.config({ path: path.join(process.cwd(), ".env") });
-
-const port = process.env.PORT || 3000;
-
-const { app } = new App();
-
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
-
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION! Shutting down...");
-  server.close(() => {
+(() => {
+  process.on("uncaughtException", (err) => {
+    console.log("UNCAUGHT EXCEPTION! Shutting down...");
+    console.log(`${err.name}: ${err.message}`);
     process.exit(1);
   });
-});
 
-process.on("SIGTERM", () => {
-  console.log("SIGTERM RECEIVED. Shutting down gracefully");
-  server.close(() => {
-    console.log("Process terminated!");
+  // Nothing in .env, just mocking...
+  dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+  const port = process.env.PORT || 3000;
+
+  const { app } = new App();
+
+  const server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
   });
-});
+
+  process.on("unhandledRejection", (err) => {
+    console.log("UNHANDLED REJECTION! Shutting down...");
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+
+  process.on("SIGTERM", () => {
+    console.log("SIGTERM RECEIVED. Shutting down gracefully");
+    server.close(() => {
+      console.log("Process terminated!");
+    });
+  });
+})();
