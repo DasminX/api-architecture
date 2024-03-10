fb9a616c7e3ce06ab95a8a70b8f9b6b5e5c1addc
DasminX <wojtek.test99@gmail.com>
2024-03-10 20:23:26 +0100

    refactor: change user service calls to call user repository injected by awilix container

--- src/features/user/service/user.service.ts
+++ src/features/user/service/user.service.ts
@@ -1,15 +1,21 @@
 import { UserFields, User } from "../model/User";
+import { UserRespositoryI } from "../repository/abstraction";
 import { UserServiceI } from "./abstraction";
 
 export class UserService implements UserServiceI {
+  private readonly userRepository: UserRespositoryI;
+
+  constructor({ userRepository }: { userRepository: UserRespositoryI }) {
+    this.userRepository = userRepository;
+  }
+
   async createUser(fields: UserFields): Promise<User> {
-    return new User({ name: "a", surname: "b", age: 33, email: "d@op.pl" });
-    // return new User(fields);
+    return this.userRepository.createUser(fields);
   }
   async getUserById(id: string): Promise<User | null> {
-    return null;
+    return this.userRepository.getUserById(id);
   }
   async getUsers(): Promise<User[]> {
-    return [new User({ name: "a", surname: "b", age: 33, email: "d@op.pl" })];
+    return this.userRepository.getUsers();
   }
 }
--- src/features/user/userContainer.ts
+++ src/features/user/userContainer.ts
@@ -3,12 +3,14 @@ import { UserRoute } from "./route/user.route";
 import { UserController } from "./controller/user.controller";
 import { UserService } from "./service/user.service";
 import { User } from "./model/User";
+import { InMemoryUserRepository } from "./repository/inMemory.repository";
 
 export const injectUserContainerDependencies = (container: AwilixContainer) => {
   container.register({
     userRoute: asClass(UserRoute).singleton(),
     userController: asClass(UserController).singleton(),
     userService: asClass(UserService).singleton(),
+    userRepository: asClass(InMemoryUserRepository).singleton(),
     userModel: asValue(User),
   });
 };
