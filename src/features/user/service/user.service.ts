6a3c80c5cf2edb236ab129d3bc4ecbfa69363fc8
DasminX <wojtek.test99@gmail.com>
2024-03-10 21:07:52 +0100

    chore: cosmetic changes

--- src/features/user/service/user.service.ts
+++ src/features/user/service/user.service.ts
@@ -12,9 +12,11 @@ export class UserService implements UserServiceI {
   async createUser(fields: UserFields): Promise<User> {
     return await this.userRepository.createUser(fields);
   }
+
   async getUserById(id: string): Promise<User | null> {
     return (await this.userRepository.getUserById(id)) ?? null;
   }
+
   async getUsers(): Promise<User[]> {
     return await this.userRepository.getUsers();
   }
