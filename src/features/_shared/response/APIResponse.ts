420d26d8e32d888c88ea3f136861f72d7958cc3e
DasminX <wojtek.test99@gmail.com>
2024-03-11 18:34:42 +0100

    feat: add new APIResponseFail and wrap errorController with it

--- src/features/_shared/response/APIResponse.ts
+++ src/features/_shared/response/APIResponse.ts
@@ -1,12 +1,11 @@
-// import { AppError } from "../../../utils/errors";
-
 export class APIResponseSuccess {
   public readonly ok = true;
 
   constructor(public readonly data: unknown) {}
 }
 
-/* export class APIResponseFail {
+export class APIResponseFail {
   public readonly ok = false;
-  public readonly reason: unknown = new AppError("Action failed!", 400);
-} */
+
+  constructor(public readonly error: unknown) {}
+}
